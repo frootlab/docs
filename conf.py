623c9699a4e309eddb00f2a6c07861fa695ebc14
Patrick Michl <patrick.michl@gmail.com>
2019-03-25 09:07:09 +0100

    fixes #1

---
+++ conf.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 Frootlab
+#
+# This file is part of Frootlab Docs, https://docs.frootlab.org
+#
+#  This file is free software: you can redistribute it and/or modify it under
+#  the terms of the GNU General Public License as published by the Free Software
+#  Foundation, either version 3 of the License, or (at your option) any later
+#  version.
+#
+#  This file is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#  A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License along with
+#  this file. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Configuration file for the Sphinx documentation builder."""
+
+__copyright__ = '2019 Frootlab'
+__license__ = 'GPLv3'
+__docformat__ = 'google'
+__author__ = 'Frootlab Developers'
+__email__ = 'contact@frootlab.org'
+__authors__ = ['Patrick Michl <patrick.michl@frootlab.org>']
+
+# Define module variables used by Sphinx
+copyright = __copyright__
+author = __author__
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.githubpages',
+    'sphinx.ext.intersphinx']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames. You can also specify multiple suffices as
+# a list of strings.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = None
+
+# The theme to use for HTML and HTML Help pages. See the documentation for a
+# list of builtin themes.
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = f'frootlabdoc'
---
+++ index.rst
@@ -0,0 +1,7 @@
+Frootlab Docs
+=============
+
+`Nemoa <http://docs.frootlab.org/nemoa>`_
+`Pandora <http://docs.frootlab.org/pandora>`_
+`Motley <http://docs.frootlab.org/motley>`_
+`flib <http://docs.frootlab.org/flib>`_
